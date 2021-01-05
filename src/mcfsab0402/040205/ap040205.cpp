ead08b0f6f52f75b322a0a7dbf3426388a9fa283
jedichou <jedigit@163.com>
2021-01-05 18:01:13 +0800

    提交示例040205

---
+++ src/mcfsab0402/040205/ap040205.cpp
@@ -0,0 +1,12 @@
+// file: ap040205.cpp
+// g++ -std=c++11 -Wall ap040204.cpp -o ap040205
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
